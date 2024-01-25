2eacc633f7eeb5e8bdd564b308c01e26d4caa601
joacoguzmanz <joaquingusman77@gmail.com>
2024-01-25 17:10:36 +0100

    deleted projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,19 @@
 import Section from "@/components/Section";
+import Link from 'next/link';
 
 const Home = async () => {
   return (
     <main>
         <Section classes={'jg-home-hero'}>
             <div className="jg-container">
-                <h1 className="jg-heading-1">I’m Joaquin! Frontend developer and Web Designer based in Barcelona.</h1>
+                <h1 className="jg-heading-extra">I’m Joaquin! Frontend developer and Web Designer based in Barcelona.</h1>
             </div>
         </Section>
 
         <Section classes={'jg-home-projects-about'}>
             <div className='jg-container'>
-                {/*<div className='jg-home-projects-container'>
-                    <h2 className='jg-heading-2'>Projects</h2>
-                </div>*/}
-
                 <div className='jg-home-about'>
                     <h2 className='jg-heading-2 jg-home-about-title'>About me</h2>
-{/*                    <p className='jg-text-1'>Lorem ipsum dolor sit amet consectetur. Rutrum rutrum purus rutrum amet.
-                        Molestie ultrices vitae sollicitudin porttitor tempor amet vestibulum massa. Arcu odio ornare
-                        lacus nunc risus turpis. Pellentesque lectus lacinia phasellus augue urna egestas nisl ac.</p>*/}
                     <p className='jg-text-1'>
                         I'm a website designer and front-end developer from Buenos Aires currently based in Barcelona.
                         I love to design minimalistic, easy-to-use and SEO friendly websites.
@@ -32,6 +26,7 @@ const Home = async () => {
             <div className='jg-container'>
                 <div className={'jg-home-blog-upper'}>
                     <h2 className='jg-heading-2'>Some insights</h2>
+                    <Link href={'#'}>Read all</Link>
                 </div>
                 <div className={'jg-home-blog-down'}>
 
